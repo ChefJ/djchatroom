21475ecc629e13b74ddc99b7a670e13f9720dc48
PIGSY <pigsy8@gmail.com>
2025-03-27 20:59:43 +0100

    try to fix the sync of gpt

--- chatmain/consumers.py
+++ chatmain/consumers.py
@@ -1,5 +1,7 @@
 # chat/consumers.py
 import json
+import threading
+
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
@@ -24,6 +26,13 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name, self.channel_name
         )
 
+    def handle_gpt_response(self, message):
+        gpt_rsp = ask_gpt(message)
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {"type": "chat.message", "message": "GPT:" + gpt_rsp}
+        )
+
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
@@ -33,10 +42,8 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name, {"type": "chat.message", "message": message}
         )
 
-        gpt_rsp = ask_gpt(message)
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat.message", "message": "GPT:"+gpt_rsp}
-        )
+        if self.room_group_name == 'gpt':
+            threading.Thread(target=self.handle_gpt_response, args=(message,)).start()
 
         # Receive message from room group
     def chat_message(self, event):
