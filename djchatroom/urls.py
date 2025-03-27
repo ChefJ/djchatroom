029279786214c9579843e75b05b7242c0d4e9c27
PIGSY <pigsy8@gmail.com>
2025-03-27 19:09:47 +0100

    updated url

--- djchatroom/urls.py
+++ djchatroom/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("chat/", include("chatmain.urls")),
+    path("/", include("chatmain.urls")),
 ]
