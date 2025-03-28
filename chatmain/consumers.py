3853639e1401754c22a4f399d8ab1a7073574f3c
PIGSY <pigsy8@gmail.com>
2025-03-28 08:37:44 +0100

    full async

--- chatmain/consumers.py
+++ chatmain/consumers.py
@@ -4,14 +4,14 @@ import threading
 import uuid
 
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from django.conf import settings
 from django.utils.timezone import now
 
 from utils.utils_vis import ask_gpt, text_to_score, generate_sentiment_graph
 
 
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
