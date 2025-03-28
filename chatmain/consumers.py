c99860c223591d32d376748d055b2719371ff04b
PIGSY <pigsy8@gmail.com>
2025-03-28 08:36:43 +0100

    full async

--- chatmain/consumers.py
+++ chatmain/consumers.py
@@ -17,12 +17,9 @@ class ChatConsumer(WebsocketConsumer):
         self.room_group_name = f"chat_{self.room_name}"
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
-
-        self.accept()
+        await self.accept()
 
     def get_client_ip(self):
         x_forwarded_for = self.scope["headers"]
