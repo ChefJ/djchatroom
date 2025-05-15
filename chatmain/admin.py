fc6dc46e815d64e858053aff311184c793e7b698
PIGSY <pigsy8@gmail.com>
2025-05-15 16:03:54 +0200

    added admin page

--- chatmain/admin.py
+++ chatmain/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(AnonymousParticipant)
+admin.site.register(OneExperiment)
+admin.site.register(ChatRoom)
+admin.site.register(ChatMessage)
